e8484b665923979e0e3a4775d5ede4396274dc6d
Ellen-jane <1368712196@qq.com>
2025-11-25 21:31:15 +0800

    故事板页，分镜详情页，秦世坤

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
 }
 
 android {
+
+
     namespace = "com.bytedance.storymagician"
     compileSdk {
         version = release(36)
@@ -51,6 +53,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -59,6 +64,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("androidx.navigation:navigation-compose:2.7.3")
 
     // 如果需要 Material3
     implementation("androidx.compose.material3:material3:1.1.2")
